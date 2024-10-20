57ba23e1961797d17e93e5f5260f2c43aaae74a3
Udo <git@udo.codes>
2024-10-20 11:54:27 +0200

    Add back `GPIOMEM`

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi2.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
+  @github_organization "optikfluffel"
   @app :nerves_system_rpi2
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
